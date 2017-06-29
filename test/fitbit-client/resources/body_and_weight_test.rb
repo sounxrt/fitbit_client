eaeaedf38ece60a79752925914199a3eab2f65b4
Michel Ocon <soun.xrt@gmail.com>
2017-06-29 18:53:03 -0500

    first test for weight and vcr cassette

---
+++ test/fitbit-client/resources/body_and_weight_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+module FitbitClient
+  module Resources
+    class BodyAndWeightTest < Minitest::Test
+      def client
+        @client ||= FitbitClient::Client.new('token', 'refresh_token', client_id: '1234567', client_secret: 'qwertyuiopoiuytr123456')
+      end
+
+      def test_weight_logs
+        VCR.use_cassette('weight_logs') do
+          response = client.weight_logs(Date.today)
+          refute_nil response['weight']
+          assert_empty response['weight']
+        end
+      end
+    end
+  end
+end
