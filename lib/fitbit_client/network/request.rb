26e7fac58732dfe4e6b9748582657e1452a692eb
Michel Ocon <soun.xrt@gmail.com>
2017-07-12 14:19:47 -0500

    Error reporting improvement, fix code climate warnings

--- lib/fitbit_client/network/request.rb
+++ lib/fitbit_client/network/request.rb
@@ -49,12 +49,7 @@ module FitbitClient
             oauth2_refresh_token!
             retry if attempt < 2
           end
-
-          # Unrecoverable token error
-          if invalid_refresh_token_error?(parsed_response) || invalid_access_token_error?(parsed_response)
-            raise FitbitClient::TokenError.new('Invalid token', e.response)
-          end
-
+          check_unrecoverable_token_error(parsed_response, e)
           raise FitbitClient::Error.new('Error during OAuth2 request', e.response)
         end
       end
@@ -70,22 +65,22 @@ module FitbitClient
         JSON.parse response.body
       end
 
+      def check_unrecoverable_token_error(parsed_response, error)
+        if invalid_token_error?(parsed_response, 'invalid_grant', 'Refresh token invalid') ||
+            invalid_token_error?(parsed_response, 'invalid_token', 'Access token invalid')
+          raise FitbitClient::TokenError.new('Invalid token', error.response)
+        end
+      end
+
       def expired_token_error?(parsed_response)
         parsed_response.dig('errors', 0, 'errorType') == 'expired_token'
       end
 
-      def invalid_access_token_error?(parsed_response)
+      def invalid_token_error?(parsed_response, error_type, error_message)
         message = parsed_response.dig('errors', 0, 'message')
         return unless message
-        parsed_response.dig('errors', 0, 'errorType') == 'invalid_token' &&
-          message.start_with?('Access token invalid')
-      end
-
-      def invalid_refresh_token_error?(parsed_response)
-        message = parsed_response.dig('errors', 0, 'message')
-        return unless message
-        parsed_response.dig('errors', 0, 'errorType') == 'invalid_grant' &&
-          message.start_with?('Refresh token invalid')
+        parsed_response.dig('errors', 0, error_type) == 'invalid_token' &&
+          message.start_with?(error_message)
       end
 
       def oauth2_refresh_token!
